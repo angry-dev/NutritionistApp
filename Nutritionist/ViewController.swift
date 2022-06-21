2a59192b09842f76974bd140aa3cd06374837a79
angry-dev <angrydev@naver.com>
2022-06-21 15:55:16 +0900

    Initial Commit

---
+++ Nutritionist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nutritionist
+//
+//  Created by 전혜성 on 2022/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
